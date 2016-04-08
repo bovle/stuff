e86a5d79fa2da49337a5f1ada4c1a8d9f45fda73
bovle <frederik.bl@live.dk>
2016-04-08 16:32:07 +0200

    work work work work work

---
+++ WebSites/ImdbBrowser - Knockout/App.js
@@ -0,0 +1,59 @@
+﻿var api = "http://www.omdbapi.com/?";
+var id = "nothing";
+
+$(document).on("pagecreate", "#page1", function () {
+    ko.applyBindings(new page1ViewModel(), document.getElementById('page1'));
+});
+
+function page1ViewModel() {
+    var self = this;
+    self.search = ko.observable();
+    self.searchData = ko.observableArray();
+    self.search.subscribe(function (newValue) {
+        $.getJSON(api, { s: newValue }, function (data) {
+            self.searchData(data.Search);          
+        });
+    });
+
+    self.movieClicked = function (movie) {
+        id = movie.imdbID;
+        $.mobile.changePage("#page2");
+    }
+    self.doNothing = function () {}
+};
+
+
+$(document).on("pagecreate", "#page2", function () {
+    ko.applyBindings(new page2ViewModel, document.getElementById('page2'));
+});
+
+$(document).on("pagebeforeshow", "#page2", function () {
+    var self = ko.dataFor($("#page2")[0]);
+    $.getJSON(api, { i: id }, function (data) {
+        self.Title(data.Title);
+        self.Year(data.Year);
+        self.imdbRating(data.imdbRating);
+        self.RunTime(data.Runtime);
+        self.Genre(data.Genre);
+        self.Released(data.Released);
+        self.Poster(data.Poster);
+    });
+});
+
+function page2ViewModel() {
+    var self = this;
+    self.Title = ko.observable();
+    self.Year = ko.observable();
+    self.imdbRating = ko.observable();
+    self.RunTime = ko.observable();
+    self.Genre = ko.observable();
+    self.Released = ko.observable();
+    self.Poster = ko.observable();
+
+    self.TitleWithYear = ko.computed(function () {
+        return self.Title() + " (" + self.Year() + ")";
+    });
+    self.Content = ko.computed(function () {
+        return self.imdbRating() + "/10 | " + self.RunTime() + " | " + self.Genre() + " | " + self.Released()
+    });
+};
---
+++ WebSites/ImdbBrowser/App.js
@@ -0,0 +1,40 @@
+﻿var api = "http://www.omdbapi.com/?"
+
+$(document).on('pagecreate', '#page1', function () {
+    $("#search-input").keyup( function () {
+        $.getJSON(api, { s: $("input:first").val() }, function (data) {
+
+            $("#search-results").html("");
+            $.each(data.Search, function (key, val) {
+                $("#search-results").append("<li class='" + val.Type + "'><a id='" + val.imdbID + "' href='#'><img src='" + val.Poster + "'/><h1>" + val.Title + "</h1><p>Year: " + val.Year + "</p></a></li>");
+            });
+
+            $("#search-results").listview({
+                autodividers: true,
+                autodividersSelector: function (li) {
+                    var out = li.attr("class");
+                    return out;
+                }
+            }).listview("refresh");
+
+            $("a").click( function (event) {
+                event.preventDefault();
+                sessionStorage.selectedId = $(this).attr("id");
+                $.mobile.changePage("#page2");
+            });
+        });      
+    });
+
+    $("#form").on("submit", function (event) {
+        event.preventDefault();
+    });
+});
+
+$(document).on("pageshow", "#page2", function () {
+
+    $.getJSON(api, { i: sessionStorage.selectedId }, function (data) {
+        $("#Title").text(data.Title + " (" + data.Year + ")");
+        $("#Content").text(data.imdbRating + "/10 | " + data.Runtime + " | " + data.Genre + " | " + data.Released)
+        $("#Poster").attr("src", data.Poster);
+    });
+});
